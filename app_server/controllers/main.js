9bad21e1dd24ec7a33a1463cf3e935ed3954fcf9
TaeyoungYoon <hijava4@gmail.com>
2014-03-14 15:05:05 +0900

    Moved data from all views to the controllers

--- app_server/controllers/main.js
+++ app_server/controllers/main.js
@@ -3,7 +3,10 @@
  */
 
 module.exports.about = function(req, res){
-  res.render('generic-text', { title: 'About' });
+  res.render('generic-text', { 
+    title: 'About',
+    content: 'Loc8r was created to help people find places to sit down and get a bit of work done.\n\nLorem ipsum dolor sit amet, consectetur adipiscing elit. Nunc sed lorem ac nisi dignissim accumsan.\n\n MEAN stack 테스트용 페이지 입니다.'
+  });
 };
 
 /*
